ed1eb2b5919e016aee14f21da02ee037496c42d7
Keith L <klowry2001@gmail.com>
2021-09-04 21:48:11 -0400

    Update CopeListener.java

--- src/main/java/lolcatloyal/IntroBot/listeners/CopeListener.java
+++ src/main/java/lolcatloyal/IntroBot/listeners/CopeListener.java
@@ -8,7 +8,6 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
-import javax.xml.soap.Text;
 import java.util.*;
 
 /**
@@ -35,18 +34,18 @@ public class CopeListener extends ListenerAdapter {
 
     private static final String DISABLED_RESPONSE = "Not coping.";
 
+    private final Random r;
+
     private boolean enabled;
 
-    private final List<String> reactionGifs;
+    //private final List<String> reactionGifs;
 
     /**
      * Creates a new CopeListener.
      */
     public CopeListener(){
-        reactionGifs = new LinkedList<String>();
+        r = new Random();
         enabled = true;
-
-        Collections.addAll(reactionGifs, DEFAULT_GIFS); //add default gifs to gif pool
     }
 
     /**
@@ -83,138 +82,51 @@ public class CopeListener extends ListenerAdapter {
             else if(messageRaw.contains("cope help")){
                 channel.sendMessage(HELP_RESPONSE).queue();
             }
-            //standard cope functions when enabled
+            //cope response
             else if (enabled){
-                if(messageRaw.contains("cope add")){
-                    String tenorLink = messageRaw.substring(8);
+                //TODO: make function for getting user mentions into a string from a message
 
-                    if (addGif(tenorLink, channel)) {
-                        channel.sendMessage("Gif added!").queue();
-                    }
-                    else {
-                        channel.sendMessage("Gif not added").queue();
-                    }
-                }
-//                else if(messageRaw.contains("cope remove")){
-//                    String tenorLink = messageRaw.substring(8);
-//
-//                    if(removeGif(tenorLink)){
-//                        channel.sendMessage("Gif removed!").queue();
-//                    }
-//                    else {
-//                        channel.sendMessage("Gif to remove not valid").queue();
-//                    }
-//                }
-                //cope response
-                else {
-                    //TODO: make function for getting user mentions into a string from a message
+                String response = COPE_RESPONSE;
 
-                    String response = COPE_RESPONSE;
+                List<User> mentionedUsers = message.getMentionedUsers();
 
-                    List<User> mentionedUsers = message.getMentionedUsers();
+                if (!mentionedUsers.isEmpty()){
+                    StringBuilder s = new StringBuilder();
 
-                    if (!mentionedUsers.isEmpty()){
-                        StringBuilder s = new StringBuilder();
+                    boolean firstMention = true;
 
-                        boolean firstMention = true;
-
-                        for (User user: mentionedUsers){
-                            if(firstMention){
-                                s.append(user.getAsMention());
-                                firstMention = false;
-                            }
-                            else {
-                                s.append(" ");
-                                s.append(user.getAsMention());
-                            }
+                    for (User user: mentionedUsers){
+                        if(firstMention){
+                            s.append(user.getAsMention());
+                            firstMention = false;
                         }
-
-                        s.append(" ");
-                        s.append(COPE_RESPONSE);
-                        response = s.toString();
+                        else {
+                            s.append(" ");
+                            s.append(user.getAsMention());
+                        }
                     }
 
-                    channel.sendMessage(response).queue();
-
-                    channel.sendMessage(chooseGif()).queue();
+                    s.append(" ");
+                    s.append(COPE_RESPONSE);
+                    response = s.toString();
                 }
+
+                channel.sendMessage(response).queue();
+                channel.sendMessage(chooseGif()).queue();
             }
         }
     }
 
     /**
      * Choose a random Tenor gif link from
-     * the list of reaction gifs.
+     * the array of reaction gifs.
      *
      * @return A randomly chosen reaction gif link.
      */
     private String chooseGif(){
-        Random r = new Random();
-        int index = r.nextInt(reactionGifs.size());
+        int i = r.nextInt(DEFAULT_GIFS.length);
 
-        return reactionGifs.get(index);
-    }
-
-    /**
-     * Adds a new tenor gif link to the list
-     * of reaction gifs if the link is valid
-     * and not already in the list.
-     *
-     * @param tenorLink The tenor link for the gif to be
-     *                  added to the list of reaction gifs.
-     * @return True if the tenor link is valid and not
-     *         already in the list of reaction gifs.
-     */
-    private boolean addGif(String tenorLink, TextChannel channel){
-        if(tenorLinkIsValid(tenorLink, channel)){
-            if (reactionGifs.add(tenorLink)){
-                channel.sendMessage("List could add the link").queue();
-                return true;
-            }
-            else {
-                channel.sendMessage("List could not add the link").queue();
-                return false;
-            }
-        }
-        else {
-            channel.sendMessage("Gif link not valid").queue();
-            return false;
-        }
-    }
-
-    /**
-     * Removes a tenor gif link from the list
-     * of reaction gifs if the link is valid and
-     * is already in the list.
-     *
-     * @param tenorLink The tenor link for the gif to be
-     *                  removed from the list of reaction gifs.
-     * @return True if the tenor link is valid and already
-     *         in the list of reaction gifs.
-     */
-//    private boolean removeGif(String tenorLink){
-//        if(!tenorLinkIsValid(tenorLink)){
-//            return false;
-//        }
-//        else{
-//            return reactionGifs.remove(tenorLink);
-//        }
-//    }
-
-    /**
-     * Checks whether a given tenor link is valid.
-     *
-     * A tenor link is considered valid if it is nonnull
-     * and contains the tenor.com/view url.
-     *
-     * @param tenorLink The String tenor link to be checked for
-     *                  validity.
-     * @return True if the tenor link is valid.
-     */
-    private boolean tenorLinkIsValid(String tenorLink, TextChannel channel){
-        String linkPrefix = tenorLink.substring(0, 22);
-
-        return linkPrefix.equals("https://tenor.com/view");
+        return DEFAULT_GIFS[i];
     }
 
     /**
