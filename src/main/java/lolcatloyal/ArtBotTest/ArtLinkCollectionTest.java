bb2fa034c40696f6f3df0315ec6aa4e425839e85
klowry <klowry@vt.edu>
2022-03-21 16:01:01 -0400

    Added Test Class for ArtLinkCollection

---
+++ src/main/java/lolcatloyal/ArtBotTest/ArtLinkCollectionTest.java
@@ -0,0 +1,28 @@
+package lolcatloyal.ArtBotTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtLinkCollectionTest {
+
+    @Test
+    void getArtistLinks() {
+    }
+
+    @Test
+    void getArtLinks() {
+    }
+
+    @Test
+    void addArtLink() {
+    }
+
+    @Test
+    void removeArtLink() {
+    }
+
+    @Test
+    void removeArtistLink() {
+    }
+}
